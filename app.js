13b5d4817d11f9bcd240cbe68e2e1f1a77b1af70
Benjamin Zhuo <benjamin.zhuo@stonybrook.edu>
2018-07-13 17:21:34 +0000

    functionality hard coded

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
-var searchRequest = {
-  term:'chinese',
-  location:'11204'
-};
+
+
 app.use(express.static(__dirname + '/public'));
 const yelp = require('yelp-fusion');
 const apiKey = 'IduwTCt23YrUfM6ItMYTUpMQ2m4EnFnKoe864OhlysThSnYan0uIY1O_YsudcXPi0fnhZQMYS52Md6sfjBaXCdtQr_nic9LJtuo7BUMjIB1SaBP9X0uvXm9ACuZHW3Yx';
@@ -18,14 +16,23 @@ app.get("/", function(req, res){
 
 app.get("/results", function(req, res){
     var firstResult;
+    console.log(req.query.food);
+    console.log(req.query.loc);
+    var searchRequest = {
+        term:req.query.food,
+        location:req.query.loc
+    };
+    var data;
     client.search(searchRequest).then(response => {
         firstResult = response.jsonBody.businesses[1];
-        const prettyJson = JSON.stringify(firstResult, null, 4);
-        console.log(prettyJson);
+        var string = JSON.stringify(firstResult, null, 4);
+        data = JSON.parse(string);
+        res.render("results", {data: data});
+        console.log(data);
     }).catch(e => {
         console.log(e);
     });
-    res.render("results");
+    
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
